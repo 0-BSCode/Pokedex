16503e4966433c21db0655ad51a379aabc43589a
0-BSCode <bryanipsanchez@gmail.com>
2024-04-15 20:33:04 +0800

    Add button UI

--- src/components/sortForm/index.tsx
+++ src/components/sortForm/index.tsx
@@ -5,7 +5,7 @@ import { FilterCriteriaEnum } from "@/types/enums/FilterCriteriaEnum"
 import { SortOrderEnum } from "@/types/enums/SortOrderEnum"
 
 const SortForm = (): ReactElement => {
-  const { sortFilterCriteria, setSortFilterCriteria, setSortOrder } =
+  const { sortFilterCriteria, setSortFilterCriteria, sortOrder, setSortOrder } =
     useFilterStore()
   return (
     <div className="flex flex-col gap-2">
@@ -36,50 +36,32 @@ const SortForm = (): ReactElement => {
         <div className="flex flex-col gap-2">
           {/* TODO: Replace w/ smthg more user friendly */}
           <button
+            type="button"
+            className={
+              sortOrder === SortOrderEnum.ASC
+                ? "py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+                : "py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:bg-blue-100 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:bg-blue-800/30 dark:hover:text-blue-400"
+            }
             onClick={() => {
               setSortOrder(SortOrderEnum.ASC)
             }}
-            disabled={
-              !sortFilterCriteria ||
-              sortFilterCriteria === FilterCriteriaEnum.ID
-            }
+            disabled={!sortFilterCriteria}
           >
-            Sort by name ASC
+            Ascending
           </button>
           <button
+            type="button"
+            className={
+              sortOrder === SortOrderEnum.DESC
+                ? "py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+                : "py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:bg-blue-100 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:bg-blue-800/30 dark:hover:text-blue-400"
+            }
             onClick={() => {
               setSortOrder(SortOrderEnum.DESC)
             }}
-            disabled={
-              !sortFilterCriteria ||
-              sortFilterCriteria === FilterCriteriaEnum.ID
-            }
+            disabled={!sortFilterCriteria}
           >
-            Sort by name DESC
-          </button>
-        </div>
-        <div className="flex flex-col gap-2">
-          <button
-            onClick={() => {
-              setSortOrder(SortOrderEnum.ASC)
-            }}
-            disabled={
-              !sortFilterCriteria ||
-              sortFilterCriteria === FilterCriteriaEnum.NAME
-            }
-          >
-            Sort by ID ASC
-          </button>
-          <button
-            onClick={() => {
-              setSortOrder(SortOrderEnum.DESC)
-            }}
-            disabled={
-              !sortFilterCriteria ||
-              sortFilterCriteria === FilterCriteriaEnum.NAME
-            }
-          >
-            Sort by ID DESC
+            Descending
           </button>
         </div>
       </div>
